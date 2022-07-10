49ac80289351b7d72b940ed0cd5a0b3118939feb
Bednarek Mikołaj <mikolaj@bednarek.xyz>
2022-07-10 13:13:34 +0200

    added tools to a new build system configuration

--- cmake/configure.sh
+++ cmake/configure.sh
@@ -13,3 +13,10 @@ for serv in $services; do
   echo "Configuring service $serv"
   ./cmake/configure-library.sh $serv "service/$serv" "executable"
 done
+
+tools=$(jq -r '.tools[]' <Build.json)
+
+for tool in $tools; do
+  echo "Configuring tool $tool"
+  ./cmake/configure-library.sh $tool "tool/$tool" "executable"
+done
